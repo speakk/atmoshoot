5210d6ff40c1ab5bc2231f50946445cdefbb8fa9
Jesse Viikari <jesse.viikari@vincit.fi>
2021-05-09 19:38:17 +0300

    Ai only follow players

--- src/systems/basic_ai.rs
+++ src/systems/basic_ai.rs
@@ -1,17 +1,21 @@
 use bevy::prelude::*;
 
-use crate::components::{BasicAi, FollowEntity};
+use crate::components::{BasicAi, FollowEntity, Player};
 use crate::events::EntityNoticed;
 
 pub fn basic_ai_notice_entity(
     query: Query<(Entity, &BasicAi)>,
+    player_query: Query<&Player>,
     mut notice_events: EventReader<EntityNoticed>,
     mut commands: Commands,
 ) {
     for notice_event in notice_events.iter() {
         query
             .iter()
-            .filter(|(entity, _)| *entity == notice_event.observer_entity)
+            .filter(|(entity, _)| {
+                *entity == notice_event.observer_entity
+                    && player_query.get(notice_event.noticed_entity).is_ok()
+            })
             .for_each(|(entity, _)| {
                 commands.entity(entity).insert(FollowEntity {
                     target: notice_event.noticed_entity,
